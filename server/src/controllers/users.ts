e6892c87297defb7447325649ba89e08d289f9f4
Sam <leblanc.SAL1@gmail.com>
2024-11-17 08:48:21 -0400

    added route to get users

---
+++ server/src/controllers/users.ts
@@ -0,0 +1,19 @@
+import UserModel from "../models/User";
+import { Request, Response } from "express";
+
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    if (req.query.search) {
+      const { search } = req.query;
+      const users = await UserModel.find({
+        username: { $regex: search, $options: "i" },
+      });
+      res.status(200).json(users);
+      return;
+    }
+    const users = await UserModel.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db";
 import authRouter from "./routes/auth";
+import userRouter from "./routes/users";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(cors());
 connectDB();
 
 app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
