6bb65ec8ae646360edd4f4c11e30f1e1a34c4a00
Sam <leblanc.SAL1@gmail.com>
2024-11-19 18:16:56 -0400

    added useDebounce hook

--- client/src/components/CreateChatModal.tsx
+++ client/src/components/CreateChatModal.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import axios from "axios";
+import useDebounce from "../hooks/useDebounce.tsx";
 import pfp from "../assets/images/pfp.png";
 
 type CreateChatModalProps = {
@@ -9,23 +10,26 @@ type CreateChatModalProps = {
 
 export default function CreateChatModal({ setIsOpen }: CreateChatModalProps) {
   const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search);
   const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function getResults(value: string) {
     if (value === "") {
       setResults([]);
       return;
     }
+    setIsLoading(true);
     axios
       .get(`${import.meta.env.VITE_SERVER}/api/users?search=${value}`)
       .then((res) => setResults(res.data))
-      .catch((err) => console.error(err));
+      .catch((err) => console.error(err))
+      .finally(() => setIsLoading(false));
   }
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setSearch(e.target.value);
-    getResults(e.target.value);
-  }
+  useEffect(() => {
+    getResults(debouncedSearch);
+  }, [debouncedSearch]);
 
   return createPortal(
     <div
@@ -50,23 +54,30 @@ export default function CreateChatModal({ setIsOpen }: CreateChatModalProps) {
             type="search"
             placeholder="Search username"
             value={search}
-            onChange={handleChange}
+            onChange={(e) => setSearch(e.target.value)}
             autoFocus
           />
         </div>
-        <ul className="mt-2 max-h-[50vh] overflow-y-auto">
-          {results.map((result, i) => {
-            return (
-              <li
-                key={i}
-                className="flex cursor-pointer items-center rounded-lg p-2 hover:bg-green-500 hover:text-white"
-              >
-                <img className="mr-2 h-10 w-10 rounded-full" src={pfp} alt="" />
-                <p>{result.username}</p>
-              </li>
-            );
-          })}
-        </ul>
+        {isLoading && <p className="mt-2">Loading...</p>}
+        {!isLoading && (
+          <ul className="mt-2 max-h-[50vh] overflow-y-auto">
+            {results.map((result, i) => {
+              return (
+                <li
+                  key={i}
+                  className="flex cursor-pointer items-center rounded-lg p-2 hover:bg-green-500 hover:text-white"
+                >
+                  <img
+                    className="mr-2 h-10 w-10 rounded-full"
+                    src={pfp}
+                    alt=""
+                  />
+                  <p>{result.username}</p>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     </div>,
     document.getElementById("overlays") as HTMLElement,
