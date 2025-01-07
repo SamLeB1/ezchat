374d3df93de8b4c0d7a24461008b8eead556a736
Sam <leblanc.SAL1@gmail.com>
2025-01-07 09:57:48 -0400

    added route to get user

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -1,8 +1,26 @@
 import UserModel from "../models/User";
 import { Request, Response } from "express";
 
+type GetUserRouteParams = {
+  id: string;
+};
+
 type GetUsersQueryParams = {
   search?: string;
+};
+
+export const getUser = async (
+  req: Request<GetUserRouteParams>,
+  res: Response
+) => {
+  const user = await UserModel.findById(req.params.id).select("-password");
+  if (user) {
+    res.status(200).json(user);
+    return;
+  } else {
+    res.status(404).json({ errors: [{ msg: "User not found." }] });
+    return;
+  }
 };
 
 export const getUsers = async (
@@ -14,11 +32,11 @@ export const getUsers = async (
       const { search } = req.query;
       const users = await UserModel.find({
         username: { $regex: search, $options: "i" },
-      });
+      }).select("-password");
       res.status(200).json(users);
       return;
     }
-    const users = await UserModel.find();
+    const users = await UserModel.find().select("-password");
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/users";
+import { getUsers, getUser } from "../controllers/users";
 
 const router = Router();
 
 router.get("/", getUsers);
+router.get("/:id", getUser);
 
 export default router;
