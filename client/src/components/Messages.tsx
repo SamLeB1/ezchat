7c46bb543085bf1f75a157bbf40f73179c9207f7
Sam <leblanc.SAL1@gmail.com>
2025-01-18 06:54:57 -0400

    added messages loading spinner

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "sonner";
+import ClipLoader from "react-spinners/ClipLoader";
 import useAuthContext from "../hooks/useAuthContext.tsx";
 import useChatsContext from "../hooks/useChatsContext.tsx";
 import useMessagesContext from "../hooks/useMessagesContext.tsx";
@@ -8,6 +9,7 @@ import pfp from "../assets/images/pfp.png";
 import { Message } from "../types.ts";
 
 export default function Messages() {
+  const [isLoading, setIsLoading] = useState(false);
   const [refetchCount, setRefetchCount] = useState(0);
   const { stateAuth } = useAuthContext();
   const { stateChats } = useChatsContext();
@@ -29,11 +31,15 @@ export default function Messages() {
 
   useEffect(() => {
     if (stateChats.selectedChat) {
+      setIsLoading(true);
       axios
         .get(
           `${import.meta.env.VITE_SERVER}/api/messages?chatId=${stateChats.selectedChat._id}`,
         )
-        .then((res) => dispatchMessages({ type: "SET", payload: res.data }))
+        .then((res) => {
+          dispatchMessages({ type: "SET", payload: res.data });
+          setIsLoading(false);
+        })
         .catch((err) => {
           console.error(err);
           toast.error("Chat messages couldn't be fetched. Retrying...");
@@ -42,41 +48,48 @@ export default function Messages() {
     }
   }, [stateChats.selectedChat, refetchCount]);
 
-  return (
-    <div className="flex flex-1 flex-col justify-end rounded-lg bg-white p-2 shadow">
-      <div className="overflow-y-auto" style={{ maxHeight }}>
-        {messages.map((message, i) => {
-          if (isOwnMessage(message))
-            return (
-              <div
-                key={i}
-                className="ml-auto mt-1 w-fit max-w-[75%] rounded-2xl bg-blue-200 px-2 py-1 first:mt-0"
-              >
-                <p className="break-words">{message.content}</p>
-              </div>
-            );
-          else
-            return isPfpDisplayed(i) ? (
-              <div key={i} className="mt-1 flex first:mt-0">
-                <img
-                  className="mr-1 h-8 w-8 rounded-full"
-                  src={message.sender.pfp ? message.sender.pfp : pfp}
-                  alt=""
-                />
-                <div className="w-fit max-w-[75%] rounded-2xl bg-green-200 px-2 py-1">
+  if (isLoading)
+    return (
+      <div className="flex flex-1 items-center justify-center rounded-lg bg-white shadow">
+        <ClipLoader loading={isLoading} size={96} color={"#1e3a8a"} />
+      </div>
+    );
+  else
+    return (
+      <div className="flex flex-1 flex-col justify-end rounded-lg bg-white p-2 shadow">
+        <div className="overflow-y-auto" style={{ maxHeight }}>
+          {messages.map((message, i) => {
+            if (isOwnMessage(message))
+              return (
+                <div
+                  key={i}
+                  className="ml-auto mt-1 w-fit max-w-[75%] rounded-2xl bg-blue-200 px-2 py-1 first:mt-0"
+                >
                   <p className="break-words">{message.content}</p>
                 </div>
-              </div>
-            ) : (
-              <div
-                key={i}
-                className="ml-9 mt-1 w-fit max-w-[75%] rounded-2xl bg-green-200 px-2 py-1 first:mt-0"
-              >
-                <p className="break-words">{message.content}</p>
-              </div>
-            );
-        })}
+              );
+            else
+              return isPfpDisplayed(i) ? (
+                <div key={i} className="mt-1 flex first:mt-0">
+                  <img
+                    className="mr-1 h-8 w-8 rounded-full"
+                    src={message.sender.pfp ? message.sender.pfp : pfp}
+                    alt=""
+                  />
+                  <div className="w-fit max-w-[75%] rounded-2xl bg-green-200 px-2 py-1">
+                    <p className="break-words">{message.content}</p>
+                  </div>
+                </div>
+              ) : (
+                <div
+                  key={i}
+                  className="ml-9 mt-1 w-fit max-w-[75%] rounded-2xl bg-green-200 px-2 py-1 first:mt-0"
+                >
+                  <p className="break-words">{message.content}</p>
+                </div>
+              );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
 }
